eace92ad35a4aac8f25b0ff587c0c407d36c1e2e
Miguel Antenor de Oliveira <miguelantoliv@gmail.com>
2020-09-07 22:24:28 -0300

    - Created parcelable for GithubRepositoryModel.kt - Created parcelable for GithubUserModel.kt

--- app/src/main/java/br/com/boxdelivery/github_javapop/model/GithubRepositoryModel.kt
+++ app/src/main/java/br/com/boxdelivery/github_javapop/model/GithubRepositoryModel.kt
@@ -1,9 +1,45 @@
 package br.com.boxdelivery.github_javapop.model
 
-class GithubRepositoryModel(
+import android.os.Parcel
+import android.os.Parcelable
+
+data class GithubRepositoryModel(
     val name : String,
     val description : String,
     val stargazers_count : Int,
     val forks : Int,
     val owner : GithubUserModel,
-) {}
+) : Parcelable {
+
+    constructor(parcel: Parcel) : this(
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readInt(),
+        parcel.readInt(),
+        parcel.readParcelable<GithubUserModel>(
+            GithubUserModel::class.java.classLoader
+        ) ?: GithubUserModel("", "")
+    ) {}
+
+    fun getTitle() = "${owner.login}/$name"
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeString(description)
+        parcel.writeInt(stargazers_count)
+        parcel.writeInt(forks)
+        parcel.writeParcelable(owner, flags)
+    }
+
+    override fun describeContents() = 0
+
+    companion object CREATOR : Parcelable.Creator<GithubRepositoryModel> {
+        override fun createFromParcel(parcel: Parcel): GithubRepositoryModel {
+            return GithubRepositoryModel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<GithubRepositoryModel?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
--- app/src/main/java/br/com/boxdelivery/github_javapop/model/GithubUserModel.kt
+++ app/src/main/java/br/com/boxdelivery/github_javapop/model/GithubUserModel.kt
@@ -1,6 +1,33 @@
 package br.com.boxdelivery.github_javapop.model
 
-class GithubUserModel(
-    val login : String,
-    val avatar_url : String,
-) {}
+import android.os.Parcel
+import android.os.Parcelable
+
+data class GithubUserModel(
+    val login: String,
+    val avatar_url: String,
+) : Parcelable {
+
+    constructor(source: Parcel) : this(
+        source.readString() ?: "",
+        source.readString() ?: ""
+    )
+
+    override fun describeContents() = 0
+
+    override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
+        writeString(login)
+        writeString(avatar_url)
+    }
+
+    companion object {
+        @JvmField
+        val CREATOR: Parcelable.Creator<GithubUserModel> =
+            object : Parcelable.Creator<GithubUserModel> {
+                override fun createFromParcel(source: Parcel): GithubUserModel =
+                    GithubUserModel(source)
+
+                override fun newArray(size: Int): Array<GithubUserModel?> = arrayOfNulls(size)
+            }
+    }
+}
